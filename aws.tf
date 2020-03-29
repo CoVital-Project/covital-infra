c924428316437d06818fda6b2c6bba27e25b646f
Daniel J. Pritchett <dpritchett@gmail.com>
2020-03-29 12:12:52 -0500

    Adds remote state via s3/dynamo; spikes out some s3 buckets

---
+++ aws.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "videos-sandbox" {
+  bucket = "covital-video-sandbox"
+  acl    = "public-read"
+
+  tags = {
+    Name        = "covital video sandbox bucket"
+    Environment = "sandbox"
+    Owner       = "covital backend engineering"
+    ManagedBy   = "covital-infra terraform repo"
+  }
+}
+
+resource "aws_s3_bucket" "videos-staging" {
+  bucket = "covital-video-staging"
+  acl    = "public-read-write"
+
+  tags = {
+    Name        = "covital video staging bucket"
+    Environment = "staging"
+    Owner       = "covital backend engineering"
+    ManagedBy   = "covital-infra terraform repo"
+  }
+}
+
+##### TERRAFORM STATE TRACKING #########
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket = "covital-terraform-state"
+  acl    = "private"
+
+  tags = {
+    Name        = "covital terraform state bucket"
+    Environment = "sandbox"
+    Owner       = "covital backend engineering"
+    ManagedBy   = "covital-infra terraform repo"
+  }
+}
+
+# create a dynamodb table for locking the state file
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name           = "terraform-state-lock-dynamo"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name      = "DynamoDB Terraform State Lock Table"
+    Owner     = "covital backend engineering"
+    ManagedBy = "covital-infra terraform repo"
+  }
+}
--- base.tf
+++ base.tf
@@ -1,3 +1,11 @@
 terraform {
   required_version = "~> 0.12.24"
+
+  backend s3 {
+    encrypt        = true
+    bucket         = "covital-terraform-state" # managed in aws.tf
+    dynamodb_table = "terraform-state-lock-dynamo" # managed in aws.tf
+    region         = "eu-west-1"
+    key            = "terraform-states/base/covital-infra/root"
+  }
 }
