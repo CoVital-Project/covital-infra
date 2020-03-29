56a0c7da5a6e4c1b4de2ab94fffbb0ddbc55ec05
Daniel J. Pritchett <dpritchett@gmail.com>
2020-03-29 15:11:34 -0500

    Capture heroku sandbox/staging config

--- heroku.tf
+++ heroku.tf
@@ -5,3 +5,61 @@ provider "heroku" {
   // HEROKU_EMAIL
   // HEROKU_API_KEY
 }
+
+data "heroku_team" "covital" {
+  name = "covital"
+}
+
+resource "heroku_app" "pulseox-sandbox" {
+  name   = "pulseox-sandbox"
+  region = "us"
+
+  acm = true
+
+  organization {
+    name     = data.heroku_team.covital.name
+    locked   = false
+    personal = false
+  }
+
+  config_vars = {
+    # Only use this to set non-secret configs!
+    # Set secrets via `heroku config:set` to keep them out of git
+    VIDEO_UPLOAD_BUCKET_REGION = "us-east-2"
+    NODE_ENV                   = "production"
+  }
+
+  internal_routing = false
+  stack            = "heroku-18"
+
+  buildpacks = [
+    "heroku/nodejs"
+  ]
+}
+
+resource "heroku_app" "pulseox-staging" {
+  name   = "guarded-crag-28391"
+  region = "us"
+
+  acm = true
+
+  organization {
+    name     = data.heroku_team.covital.name
+    locked   = false
+    personal = false
+  }
+
+  config_vars = {
+    # Only use this to set non-secret configs!
+    # Set secrets via `heroku config:set` to keep them out of git
+    VIDEO_UPLOAD_BUCKET_REGION = "us-east-2"
+    NODE_ENV                   = "production"
+  }
+
+  internal_routing = false
+  stack            = "heroku-18"
+
+  buildpacks = [
+    "heroku/nodejs"
+  ]
+}
