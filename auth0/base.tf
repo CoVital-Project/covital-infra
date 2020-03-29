0c5244710066129f8b7c71d360ffb37efde0697f
Daniel J. Pritchett <dpritchett@gmail.com>
2020-03-29 12:41:43 -0500

    Add note on remote state to auth0/base.tf

--- auth0/base.tf
+++ auth0/base.tf
@@ -1,3 +1,6 @@
 terraform {
   required_version = "~> 0.12.24"
 }
+
+# don't bother setting up remote state for this subfolder;
+# once it gets fixed promote this back up to the root folder and let it use that state
